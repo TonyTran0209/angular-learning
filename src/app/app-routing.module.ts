c9c8c1c1312273d63fe2d73bf66bb7e6c54a13aa
Phi Manh Tran <phi.tran@niteco.se>
2020-04-24 17:52:12 +0700

    [5. Add In-app Navigation] dashboard view & routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { DashboardComponent }   from './dashboard/dashboard.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // auto navigate to dashboard & change '/' path to '/dashboard'
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'heroes', component: HeroesComponent }, // tells the router to match that URL to path: 'heroes' and display the HeroesComponent when the URL is something like localhost:4200/heroes
 ];
 
