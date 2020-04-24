cf6493269a90c77711b1b34b4f11145279d86726
Phi Manh Tran <phi.tran@niteco.se>
2020-04-24 17:30:36 +0700

    [5. Add In-app Navigation] navigation link to Heroes list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HeroesComponent } from './heroes/heroes.component';
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }, // tells the router to match that URL to path: 'heroes' and display the HeroesComponent when the URL is something like localhost:4200/heroes
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)], // configure the router at the application's root level, and performs the initial navigation based on the current browser URL
+  exports: [RouterModule], // it will be available throughout the app
 })
 export class AppRoutingModule { }
