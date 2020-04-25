1ec58ac2c02740560a3cfde9f4afdf8f20005431
TonyTran0209 <phi.tran.0209@gmail.com>
2020-04-25 17:48:21 +0700

    [6. Get Data from a Server] Simulate a data server (get Heroes list)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
 
+// Simulate a data server (by In-memory Web API module https://github.com/angular/in-memory-web-api)
+// CLI: npm install angular-in-memory-web-api --save
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+// CLI: ng generate service InMemoryData
+import { InMemoryDataService } from './in-memory-data.service';
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -22,7 +28,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    AppRoutingModule
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // & returns simulated server responses 
+    // (Remove it when a real server is ready to receive requests)
+    // forRoot(): takes an InMemoryDataService class that primes the in-memory database.
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false,
+    }),
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
 
@@ -11,14 +12,20 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
+  // Define the heroesUrl of the form :base/:collectionName with the address of the heroes resource on the server. 
+  // Here base is the resource to which requests are made,
+  // and collectionName is the heroes data object in the in-memory-data-service.ts.
+  private heroesUrl = 'api/heroes';  // URL to web api
+
   constructor(
+    private http: HttpClient,
     private messageService: MessageService
   ) { }
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
