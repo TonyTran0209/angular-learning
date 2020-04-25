2b4743a2fbcf505a92e78dbc285640d06193f2f2
TonyTran0209 <phi.tran.0209@gmail.com>
2020-04-25 18:22:56 +0700

    [6. Get Data from a Server] Error handling

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -22,10 +23,43 @@ export class HeroService {
     private messageService: MessageService
   ) { }
 
+  // After reporting the error to the console, 
+  // the handler constructs a user friendly message
+  // and returns a safe value to the app so the app can keep working.
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
     return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
+      // catchError(): intercepts an Observable that failed. It passes the error an error handler that can do what it wants with the error.
+      // handleError(): reports the error & then returns an innocuous result so that the application keeps working.
   }
 
   getHero(id: number): Observable<Hero> {
